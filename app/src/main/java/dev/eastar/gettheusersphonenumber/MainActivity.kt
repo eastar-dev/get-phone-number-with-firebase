eb23ee6075f12c5afeb705eb5db901ffd16cdf5f
eastar Jeong <438869+eastar-dev@users.noreply.github.com>
2020-10-04 09:10:15 +0900

    first

---
+++ app/src/main/java/dev/eastar/gettheusersphonenumber/MainActivity.kt
@@ -0,0 +1,56 @@
+package dev.eastar.gettheusersphonenumber
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.Credentials
+import com.google.android.gms.auth.api.credentials.HintRequest
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.actionMasked == MotionEvent.ACTION_DOWN)
+            requestHint()
+        return super.onTouchEvent(event)
+    }
+
+    private val CREDENTIAL_PICKER_REQUEST = 1  // Set to an unused request code
+
+    // Construct a request for phone numbers and show the picker
+    private fun requestHint() {
+        val hintRequest = HintRequest.Builder()
+            .setPhoneNumberIdentifierSupported(true)
+            .build()
+        val credentialsClient = Credentials.getClient(this)
+        val intent = credentialsClient.getHintPickerIntent(hintRequest)
+        startIntentSenderForResult(
+            intent.intentSender,
+            CREDENTIAL_PICKER_REQUEST,
+            null, 0, 0, 0
+        )
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CREDENTIAL_PICKER_REQUEST ->
+                // Obtain the phone number from the result
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val credential = data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)
+                    android.util.Log.e(
+                        "phonenumber",
+                        "" + credential?.id
+                    )//  <-- will need to process phone number string
+
+                }
+            // ...
+        }
+    }
+}
